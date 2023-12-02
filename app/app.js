310b13a3ae440c1126c91f293a47f31d30536daa
spacecoffeeboy <117617635+spacecoffeeboy@users.noreply.github.com>
2023-12-02 22:24:10 +0900

    ejs to js 미들웨어

--- app/app.js
+++ app/app.js
@@ -11,6 +11,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+//ejs to js 미들웨어 : 특정 디렉토리 내의 파일들을 웹 서버를 통해 접근 가능하게 함
+app.use(express.static(`${__dirname}/src/public`));
+
 // use -> 미들웨어 등록 메서드
 app.use("/", home); 
 
