27fc467119d036db3a1242a3f02e2b2fde6ba5e4
spacecoffeeboy <117617635+spacecoffeeboy@users.noreply.github.com>
2023-12-14 07:35:16 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -2,6 +2,7 @@
 const fs = require("fs").promises;
 
 class UserStorage {
+    //user.js에 있는 정보를 불러오는 메서드
     static #getUserInfo(data, email) {
         const users = JSON.parse(data);
         const idx= users.email.indexOf(email);
@@ -12,17 +13,28 @@ class UserStorage {
         }, {});
         return userInfo;
     } 
-    
-    static getUsers(...fields){
-        // const users = this.#users;
-        const newUsers = fields.reduce((newUsers,field) => {
-            if(users.hasOwnProperty(field)){
-                newUsers[field] = users[field];
-            }
-            return newUsers;
+
+    static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if(isAll) return users;
+    const newUsers = fields.reduce((newUsers,field) => {
+    if(users.hasOwnProperty(field)){
+        newUsers[field] = users[field];
+        }
+        return newUsers;
         },{});
         return newUsers;
+    }
+
+    static getUsers(isAll, ...fields){
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUsers(data, isAll, fields);
+        })
+        .catch(console.error);
     };
+
     //사용자가 기입한 "email"값을 사용하여 사용자 정보를 가져오는 메서드
     static getUserInfo(email) {
         return fs
@@ -33,11 +45,14 @@ class UserStorage {
         .catch(console.error);
     }
  
-
-    static save(userInfo){
-        // const users = this.#users;
+    static async save(userInfo){
+        const users  = await this.getUsers(true);
+        if(users.email.includes(userInfo.email)) {
+            throw "이미 존재하는 이메일입니다.";
+        }
         users.email.push(userInfo.email);
         users.pwd.push(userInfo.pwd);
+        fs.writeFile("./src/databases/users.json", JSON.stringify(users));
         return {success: true};
     }
 }
